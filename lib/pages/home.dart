7d0a4e466792f8080574c4f9647b52cdda2b7b08
Sachito <sacho.manalo69@gmail.com>
2024-08-11 17:07:33 +0800

    initial commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
-import 'package:project/theme/theme_provider.dart';
-import 'package:provider/provider.dart';
+//import 'package:project/theme/theme_provider.dart';
+//import 'package:provider/provider.dart';
 import 'package:project/pages/settings.dart';
 
 
